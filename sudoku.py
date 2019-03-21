2cd129c31631d8ba29fcaf10db5f56ceb6f4b778
Agurato <mail.vmonot@gmail.com>
2019-03-21 23:44:06 +0100

    begin resolve brute

--- sudoku.py
+++ sudoku.py
@@ -2,6 +2,7 @@ class Cell:
    def __init__(self):
       self.possible = [True]*9
       self.value = 0
+      self.init = False
    
    def remove_possible(self, v):
       self.possible[v-1] = False
@@ -23,6 +24,8 @@ class SudokuGrid:
       if len(values) == 81:
          for i in range(0, 81):
             self.cells[i].value = values[i]
+            if values[i] != 0:
+               self.cells[i].init = True
 
    def set_value(self, value, index):
       """
@@ -71,6 +74,25 @@ class SudokuGrid:
 
    def check_cell(self, index):
       return self.check_row(index) and self.check_col(index) and self.check_square(index)
+
+   def resolve_brute(self, index, test_val):
+      go_next_cell = False
+
+      # Last cell has been processed
+      if index > 80:
+         return
+
+      if self.cells[index].init is False:
+         self.cells[index].value = test_val
+         if self.check_cell(index):
+            go_next_cell = True
+         else:
+            self.resolve_brute(index, test_val + 1)
+      else:
+         go_next_cell = True
+      
+      if go_next_cell:
+         self.resolve_brute(index + 1, 1)
 
 if __name__ == '__main__':
    grid = SudokuGrid()
