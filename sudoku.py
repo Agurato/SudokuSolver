96da81b976617cd672f1bead29454f84878968a6
Agurato <mail.vmonot@gmail.com>
2019-04-02 20:34:18 +0200

    stuff

--- sudoku.py
+++ sudoku.py
@@ -66,16 +66,41 @@ class Sudoku:
                disp += " | "
             cell = self.cells[int(y/3)*9 + int(x/3)]
             poss_index = int(y%3)*3 + int(x%3)
-            #if cell.value == 0:
             if cell.possible[poss_index]:
                disp += " " + str(poss_index+1)
             else:
                disp += " ."
-            #else:
-            #   if poss_index == 4:
-            #      disp += " "+str(cell.value)
-            #   else:
-            #      disp += "  "
+         disp += "\n"
+      print(disp)
+      sys.stdout.flush()
+
+   def display_mix(self):
+      #os.system('cls' if os.name == 'nt' else 'clear')
+      print(" ===================================================================")
+      disp = "\n"
+      for y in range(0, 27):
+         if y in [3, 6, 9, 12, 15, 18, 21, 24, 27]:
+            disp += "                      |                       |                     \n"
+         if y in [9, 18]:
+            disp += " ---------------------+-----------------------+---------------------\n"
+            disp += "                      |                       |                     \n"
+         for x in range(0, 27):
+            if x in [3, 6, 9, 12, 15, 18, 21, 24, 27]:
+               disp += " "
+            if x in [9, 18]:
+               disp += " | "
+            cell = self.cells[int(y/3)*9 + int(x/3)]
+            poss_index = int(y%3)*3 + int(x%3)
+            if cell.value == 0:
+               if cell.possible[poss_index]:
+                  disp += " " + str(poss_index+1)
+               else:
+                  disp += " ."
+            else:
+               if poss_index == 4:
+                  disp += " "+str(cell.value)
+               else:
+                  disp += "  "
          disp += "\n"
       print(disp)
       sys.stdout.flush()
@@ -170,6 +195,7 @@ class Sudoku:
          return self.resolve_brute(index + 1)
    
    def resolve_smart(self, index=0, test_val=1):
+      self.display_full()
       # Last cell has been processed
       if index > 80:
          return True
